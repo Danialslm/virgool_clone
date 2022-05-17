b0bc081dbac5b406d0ba3a4863124c92b406f105
Danial selmipoor <danial9713@gmail.com>
2022-05-17 19:47:58 +0430

    use assertIs instead assertEqual

--- apps/tags/tests/test_urls.py
+++ apps/tags/tests/test_urls.py
@@ -9,7 +9,7 @@ class TagUrlsTest(SimpleTestCase):
     def test_post_tags_url(self):
         url = reverse('tags:post_tags', kwargs={'hash': 'sample'})
         view = resolve(url)
-        self.assertEqual(
+        self.assertIs(
             view.func.view_class.__name__,
             views.PostTagsAPIView.__name__,
         )
@@ -17,7 +17,7 @@ class TagUrlsTest(SimpleTestCase):
     def test_tag_search_url(self):
         url = reverse('tags:search')
         view = resolve(url)
-        self.assertEqual(
+        self.assertIs(
             view.func.view_class.__name__,
             views.TagSearchListAPIView.__name__,
         )
@@ -25,7 +25,7 @@ class TagUrlsTest(SimpleTestCase):
     def test_tag_posts_url(self):
         url = reverse('tags:tag_posts', kwargs={'tag': 'sample tag'})
         view = resolve(url)
-        self.assertEqual(
+        self.assertIs(
             view.func.view_class.__name__,
             views.TagPostListAPIView.__name__,
         )
